a28240fe3b2ee9924e15fb2ca3cfadcb8559b2e1
cslabproject <koltdoyle@unomaha.edu>
2025-04-22 20:00:25 -0500

    Created GUI layout and basic layout function

--- logic.py
+++ logic.py
@@ -1,1 +1,7 @@
-# Handles logic for GUI
+from PyQt6.QtWidgets import *
+from gui import Ui_MainWindow
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Main that creates GUI application
+from logic import *
+
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
+
+if __name__ == "__main__":
+    main()
+
+'''
+Note: If, after running, the text isn't bold:
+
+'font.setWeight(75)' must be removed or commented out for every time text is bolded in 'gui.py' 
+For whatever reason, it breaks how bold text works, but PyQt6 puts it in anyway
+This must be repeated every time the gui.py file is updated with a gui.ui file
+'''
